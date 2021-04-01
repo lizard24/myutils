627a5eaa5972003c0f37e3357278e8c2d320fe25
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-04-01 16:02:35 +0100

    changes to setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(name='myutils',
-      package_dir = {'': 'myutils'},
+      # package_dir = {'': 'myutils'}, # if there was a folder "myutils" in the folder
       packages = find_packages(),
       )
