048f0765ecee7fe5ccc976709f0d114396019fc4
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-07-12 13:21:07 +0100

    changes to csbdeep

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='myutils',
-      version='0.0.6',
+      version='0.0.7',
       # package_dir = {'': 'myutils'}, # if there was a folder "myutils" in the folder
       packages = find_packages(),
       install_requires=[
