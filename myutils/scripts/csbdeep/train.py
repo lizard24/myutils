7e3324294e3f127300940c3805d2312c17cc9fde
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-07-21 17:43:42 +0100

    changes to train.py

--- myutils/scripts/csbdeep/train.py
+++ myutils/scripts/csbdeep/train.py
@@ -3,6 +3,8 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 from csbdeep.utils import axes_dict, plot_some, plot_history
 from csbdeep.io import load_training_data
 from csbdeep.models import Config, CARE
+from six import string_types
+
 
 from myutils.general   import copy_file
 from myutils.mycsbdeep import get_history
@@ -13,6 +15,25 @@ import numpy as np
 
 import os
 
+def plot_history_v2(history,*keys,**kwargs):
+
+    logy = kwargs.pop('logy',False)
+
+    if all(( isinstance(k,string_types) for k in keys )):
+        w, keys = 1, [keys]
+    else:
+        w = len(keys)
+
+    plt.gcf()
+    for i, group in enumerate(keys):
+        plt.subplot(1,w,i+1)
+        for k in ([group] if isinstance(group,string_types) else group):
+            plt.plot(history['epoch_no'],history[k],'.-',label=k,**kwargs)
+            if logy:
+                plt.gca().set_yscale('log', nonposy='clip')
+        plt.xlabel('epoch')
+        plt.legend(loc='best')
+        
 
 
 parser = argparse.ArgumentParser()
@@ -45,7 +66,7 @@ parser.add_argument("--train_epochs_pretrained", default=0, type=int, help='Last
 args, unknown = parser.parse_known_args()
 
 if os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name) ):
-    print("FYI: Folder already exists: '%s/%s'!" % (args.model_folder, args.model_name))
+    print("Note: Folder already exists: '%s/%s'!" % (args.model_folder, args.model_name))
 
 
 if not args.model_name_pretrained is None:
@@ -71,19 +92,19 @@ if args.continue_training:
     else:
         args.continue_training = False
 
-        
-print(args.continue_training)
 
-if not (args.model_name_pretrained is None) or (args.continue_training is True):
-    history_pre = get_history('%s/%s/history.txt' % (args.model_folder, args.model_name))
-    print("Learning rate is modified: %s -> %s !" % (args.train_learning_rate, history_pre['lr'][-1]) )
-    args.train_learning_rate = history_pre['lr'][-1]
+if os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
+    if not (args.model_name_pretrained is None) or (args.continue_training is True):
+        history_all = get_history('%s/%s/history.txt' % (args.model_folder, args.model_name))
+        if args.train_learning_rate != history_all['lr'][-1]:
+            print("Learning rate is modified: %s -> %s !" % (args.train_learning_rate, history_all['lr'][-1]) )
+            args.train_learning_rate = history_all['lr'][-1]
 else:
-    history_pre = None
+    history_all = {}
 
 
 if args.train_epochs<=0:
-    print("'train_epochs<=0' - return without training.")
+    print("'train_epochs' <= 0 - return without training.")
     sys.exit()
 
 (X,Y), (X_val,Y_val), axes = load_training_data( args.file_npz,
@@ -97,7 +118,7 @@ n_channel_in, n_channel_out = X.shape[c], Y.shape[c]
 config = Config( axes,
                  n_channel_in,
                  n_channel_out,
-                 train_tensorboard     = False,
+                 train_tensorboard     = True       if args.display else False,
                  train_loss            = 'laplace'  if args.train_loss == 'probabilistic' else args.train_loss,
                  probabilistic         = True       if args.train_loss == 'probabilistic' else False,
                  ms_ssim_no_weights    = args.ms_ssim_no_weights,
@@ -107,8 +128,7 @@ config = Config( axes,
                  train_steps_per_epoch = args.train_steps_per_epoch,
                  train_epochs          = args.train_epochs,
                  callback_period       = args.callback_period,
-                 callback_start        = args.train_epochs_pretrained
-               )
+                 callback_start        = args.train_epochs_pretrained )
 
 
 model = CARE(config, args.model_name, basedir=args.model_folder)
@@ -123,7 +143,8 @@ elif args.continue_training:
     model.load_weights('callbacks/%s' % callback_file)
     print("Weights are loaded from earlier callback: %s" % callback_file)
 
-history = model.train(X, Y, validation_data = (X_val, Y_val))
+history = model.train( X, Y, validation_data = (X_val, Y_val),
+                       verbose = 1 if args.display else 0 )  ### 0 = silent, 1 = progress bar, 2 = one line per epoch
 
 
 plt.figure(figsize=(20,12))
@@ -140,32 +161,42 @@ if args.display:
     plt.show()
 plt.close()
 
+#if not history_all=={}:
+    #history.history['epoch_no'] = [ item+history_all['epoch_no'][-1] for item in history.history['epoch_no'] ]
+for col in history.history.keys():
+    history_all[col] = history_all[col] + history.history[col] if col in history_all.keys() else history.history[col]
 
-cols =  ['epoch_no', 'loss', 'val_loss', 'mae', 'val_mae', 'mse', 'val_mse', 'ssim', 'val_ssim', 'lr']
-
-if not history_pre is None:
-    for col in cols:
-        history.history[col] = history_pre[col] + history.history[col] 
-
-        
 plt.figure(figsize=(16,5))
-#cols = list(history.history.keys())
-plot_history(history,['loss', 'val_loss'],['mse', 'val_mse', 'mae', 'val_mae', 'ssim', 'val_ssim'])
-plt.savefig( "%s/%s/loss.png" % (args.model_folder,
-                                 args.model_name) )
+plot_history_v2(history_all,['loss', 'val_loss'],['mse', 'val_mse', 'mae', 'val_mae', 'ssim', 'val_ssim'])
+plt.savefig( "%s/%s/loss.png" % (args.model_folder, args.model_name) )
 if args.display:
     plt.show()
 plt.close()
 
-if not os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
-    file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
-    file.write("%s\n" % "\t".join(cols))
-else:
-    file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
-    
-for row in range(len(history.history[cols[0]])):
-    for col in cols:
-        file.write(str(history.history[col][row])+"\t")
-    file.write("\n")
 
+cols =  ['epoch_no', 'loss', 'val_loss', 'mae', 'val_mae', 'mse', 'val_mse', 'ssim', 'val_ssim', 'lr']
+
+file = open("%s/%s/history.txt" % (args.model_folder, args.model_name), "w") ### overwrites history.txt
+file.write("%s\n" % "\t".join(cols))
+
+for row in range(len(history_all[cols[0]])):
+    file.write( "%s\n" % "\t".join( [str(history_all[col][row]) for col in cols] ) )
+    
 file.close()
+
+
+
+
+
+# if not os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
+#     file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
+#     file.write("%s\n" % "\t".join(cols))
+# else:
+#     file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
+    
+# for row in range(len(history.history[cols[0]])):
+#     for col in cols:
+#         file.write(str(history.history[col][row])+"\t")
+#     file.write("\n")
+
+# file.close()
