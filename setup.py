31b4ef45398e654004839089c2114da3cb62421b
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-07-26 18:04:52 +0100

    move train.py from myutils to 'csbdeep/myscripts'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='myutils',
-      version='0.0.14',
+      version='0.0.15',
       # package_dir = {'': 'myutils'}, # if there was a folder "myutils" in the folder
       packages = find_packages(),
       install_requires=[
