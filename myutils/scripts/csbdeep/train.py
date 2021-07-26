872325a3d7faaeb83a8a5af395f456250771244e
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-07-26 13:47:01 +0100

    add 'folder_data' option instead of 'file_npz'

--- myutils/scripts/csbdeep/train.py
+++ myutils/scripts/csbdeep/train.py
@@ -1,19 +1,35 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 
-from csbdeep.utils import axes_dict, plot_some, plot_history
-from csbdeep.io import load_training_data
-from csbdeep.models import Config, CARE
-from six import string_types
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import random
+import sys
+import os
 
+from tifffile import imread
+from six      import string_types
+
+from csbdeep.utils  import axes_dict, plot_some, plot_history
+from csbdeep.io     import load_training_data
+from csbdeep.models import Config, CARE
+from csbdeep.data   import shuffle_inplace
 
 from myutils.general   import copy_file
 from myutils.mycsbdeep import get_history
 
-import matplotlib.pyplot as plt
-import argparse
-import numpy as np
 
-import os
+
+########## FUNCTIONS
+
+def prepare_data(x,y):
+    shuffle_inplace(x,y)
+    x, y = np.asarray(x), np.asarray(y)
+    if len(x.shape) != len(axes):
+        cidx = axes.find('C')
+        cidx = axes.find('C')
+        x, y = map(lambda z: np.reshape(z, z.shape[:cidx]+(1,)+z.shape[cidx:]), (x, y))
+    return x, y
 
 def plot_history_v2(history,*keys,**kwargs):
 
@@ -24,6 +40,8 @@ def plot_history_v2(history,*keys,**kwargs):
     else:
         w = len(keys)
 
+    print(history.keys())
+    
     plt.gcf()
     for i, group in enumerate(keys):
         plt.subplot(1,w,i+1)
@@ -34,16 +52,21 @@ def plot_history_v2(history,*keys,**kwargs):
         plt.xlabel('epoch')
         plt.legend(loc='best')
         
-
+   
+        
+########## ARGUMENTS
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--file_npz", default='traig-data.npz', type=str, help="Npz file with training data.")
+parser.add_argument("--file_npz", default=None, type=str, help="Npz file with training data.")
+
+parser.add_argument("--folder_data", default=None, type=str, help="Alternative to 'file_npz'. 'folder_data' is directory to subfolders that contain training data ('input', target', 'target_val', 'input_val').")
+parser.add_argument("--axes", default='SYXC', type=str, help="Axes of training data. Needs to be defined if 'file_npz' is None.")
 
 parser.add_argument("--model_name", default='my_model', type=str, help="Name of the folder the model is saved in.")
 parser.add_argument("--model_folder", default='models', type=str, help="Folder the model is saved in.")
 
-parser.add_argument("--val_split", default=0.05, type=float, help="Percentage of validation split.")
+parser.add_argument("--val_split", default=0.1, type=float, help="Percentage of validation split.")
 
 parser.add_argument("--train_loss", default="mae", type=str, help="Loss function")
 parser.add_argument("--ms_ssim_no_weights", default=3, type=int, help="Number of weights if train_loss is ms_ssim.")
@@ -65,9 +88,12 @@ parser.add_argument("--train_epochs_pretrained", default=0, type=int, help='Last
 
 args, unknown = parser.parse_known_args()
 
+
+
+########## LOAD PREVIOUS MODELS
+
 if os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name) ):
     print("Note: Folder already exists: '%s/%s'!" % (args.model_folder, args.model_name))
-
 
 if not args.model_name_pretrained is None:
     args.continue_training = False
@@ -92,7 +118,6 @@ if args.continue_training:
     else:
         args.continue_training = False
 
-
 if os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
     if not (args.model_name_pretrained is None) or (args.continue_training is True):
         history_all = get_history('%s/%s/history.txt' % (args.model_folder, args.model_name))
@@ -100,16 +125,43 @@ if os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
             print("Learning rate is modified: %s -> %s !" % (args.train_learning_rate, history_all['lr'][-1]) )
             args.train_learning_rate = history_all['lr'][-1]
 else:
-    history_all = {}
-
+    history_all = {'epoch_no': [n+1 for n in range(args.train_epochs)]}
 
 if args.train_epochs<=0:
     print("'train_epochs' <= 0 - return without training.")
     sys.exit()
 
-(X,Y), (X_val,Y_val), axes = load_training_data( args.file_npz,
-                                                 validation_split = args.val_split,
-                                                 verbose = False )
+
+
+    
+########## GET TRAINING DATA
+    
+if not args.file_npz is None:
+    
+    (X,Y), (X_val,Y_val), axes = load_training_data( args.file_npz,
+                                                     validation_split = args.val_split,
+                                                     verbose = False )
+    
+elif not (args.folder_data is None) and (os.path.isdir(args.folder_data)):
+    
+    X     = [ imread('/'.join([args.folder_data, 'input'     , file])) for file in os.listdir('%s/input'      % args.folder_data) ]
+    Y     = [ imread('/'.join([args.folder_data, 'target'    , file])) for file in os.listdir('%s/target'     % args.folder_data) ]
+    X_val = [ imread('/'.join([args.folder_data, 'input_val' , file])) for file in os.listdir('%s/input_val'  % args.folder_data) ]
+    Y_val = [ imread('/'.join([args.folder_data, 'target_val', file])) for file in os.listdir('%s/target_val' % args.folder_data) ]
+
+    axes = args.axes
+        
+    X, Y         = prepare_data(X, Y)
+    X_val, Y_val = prepare_data(X_val, Y_val)
+    
+else:
+    print("Define 'file_npz' or 'folder_data' - return without execution.")
+    sys.exit()
+    
+
+
+
+########## START TRAINING
 
 c = axes_dict(axes)['C']
 n_channel_in, n_channel_out = X.shape[c], Y.shape[c]
@@ -147,6 +199,10 @@ history = model.train( X, Y, validation_data = (X_val, Y_val),
                        verbose = 1 if args.display else 0 )  ### 0 = silent, 1 = progress bar, 2 = one line per epoch
 
 
+
+
+########## EXPORT: FIGURES & HISTORY
+
 plt.figure(figsize=(20,12))
 _P = model.keras_model.predict(X_val[:5])
 if config.probabilistic:
@@ -161,8 +217,6 @@ if args.display:
     plt.show()
 plt.close()
 
-#if not history_all=={}:
-    #history.history['epoch_no'] = [ item+history_all['epoch_no'][-1] for item in history.history['epoch_no'] ]
 for col in history.history.keys():
     history_all[col] = history_all[col] + history.history[col] if col in history_all.keys() else history.history[col]
 
@@ -183,20 +237,3 @@ for row in range(len(history_all[cols[0]])):
     file.write( "%s\n" % "\t".join( [str(history_all[col][row]) for col in cols] ) )
     
 file.close()
-
-
-
-
-
-# if not os.path.exists('%s/%s/history.txt' % (args.model_folder, args.model_name)):
-#     file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
-#     file.write("%s\n" % "\t".join(cols))
-# else:
-#     file = open("%s/%s/history.txt" % (args.model_folder, args.model_name),"a")
-    
-# for row in range(len(history.history[cols[0]])):
-#     for col in cols:
-#         file.write(str(history.history[col][row])+"\t")
-#     file.write("\n")
-
-# file.close()
