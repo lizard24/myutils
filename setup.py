65a38a59ac46709e65edb71a6724b8625d8d170d
Lisa-admin <Lisa-admin@sipbs-lk.ds.strath.ac.uk>
2021-07-27 23:46:55 +0100

    add npz_to_folder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='myutils',
-      version='0.0.17',
+      version='0.0.18',
       # package_dir = {'': 'myutils'}, # if there was a folder "myutils" in the folder
       packages = find_packages(),
       install_requires=[
